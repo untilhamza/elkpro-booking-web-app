b657883e7c8636de19d2e071f483a67b4d1d85f4
Mpabulungi Mark <markompab@gmail.com>
2026-03-16 17:20:06 +0900

    Update map links for Kakao and Google Maps
    Business Address Changed

--- src/components/MapDirections/MapDirections.tsx
+++ src/components/MapDirections/MapDirections.tsx
@@ -5,11 +5,14 @@ import Button from "react-bootstrap/Button";
 const MapDirections = () => {
 
   const handleViewKakaoMapClick = () => {
-    window.open(`https://map.kakao.com/link/to/Elkpro services,37.535323741447456, 126.99206905398854`);
+    //window.open(`https://map.kakao.com/link/to/Elkpro services,37.535323741447456, 126.99206905398854`);
+    window.open(`https://kko.to/tZt6z9cKSc`);
   };
   const handleViewGoogleMapClick = () => {
-    window.open(`https://www.google.com/maps/search/?api=1&query=37.535323741447456, 126.99206905398854`);
+    //window.open(`https://www.google.com/maps/search/?api=1&query=37.535323741447456, 126.99206905398854`);
+    window.open(`https://www.google.com/maps/search/?api=1&query=37.543531, 126.957720`);
   };
+  
   return (
     <div className="mb-2 row p-3 gap-2">
       <Button variant="primary col" className="d-block w-100" onClick={handleViewKakaoMapClick}>
